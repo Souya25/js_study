be0ceabacc5097e3186a3283c844ec4aa4732787
Souya25 <55970079+Souya25@users.noreply.github.com>
2022-06-22 14:40:39 +0900

    fix conflict

--- practice/practice2.js
+++ practice/practice2.js
@@ -33,6 +33,36 @@ sanNameArr = nameArr.map((name) => {
   });
   console.log(sanNameArr);
 
+
+const nameArr2 = nameArr.map((name) => {
+  return name;
+});
+console.log(nameArr2)
+
+//1行で書ける！
+nameArr.map((name) => console.log(name));
+
+//filterは「配列の中から特定の条件に合うものだけを取り出したいとき」に便利！！
+const numArr1 = [1, 2, 3, 4, 5];
+const numArr2 = numArr1.filter((num) => {
+  return num%2 === 1;
+});
+
+console.log(numArr2);
+
+
+// mapとfilterは第2引数でindex情報を取得することができる
+nameArr.map((name, index) => console.log(`${name}は${index + 1}番目`));
+
+sanNameArr = nameArr.map((name) => {
+    if (name === "主田"){
+      return name;
+    }else{
+      return `${name}さん`;
+    }
+  });
+  console.log(sanNameArr);
+
 //三項演算子
 // ある条件　? 条件がTrueの時の処理 : じょうけんが　Falseの時の処理
 const val1 = 1 < 0 ? "Trueです" : "Falseです";
